1aa8482f507d78d4c8ea7ad6c36864769b89fbd3
JanetteDevCode <30553904+JanetteDevCode@users.noreply.github.com>
2019-11-15 12:18:42 -0800

    Use command-line database

---
+++ mongo.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+
+if (process.argv.length < 3) {
+  console.log('missing arguments for database password, phonebook name, and phonebook number');
+  process.exit(1);
+} else if (process.argv.length === 4) {
+  console.log('missing argument for phone number');
+  process.exit(1);
+}
+
+const dbName = 'phonebook-app';
+const dbPassword = process.argv[2];
+const name = process.argv[3];
+const phone = process.argv[4];
+
+
+const url =
+  `mongodb+srv://fullstackopen_rw:${dbPassword}@cluster0-fullstackopen-6rw1t.mongodb.net/${dbName}?retryWrites=true&w=majority`;
+
+  mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
+
+  const personSchema = new mongoose.Schema({
+    name: String,
+    phone: String
+  }, { collection: 'persons' });
+  
+  const Person = mongoose.model('Person', personSchema);
+
+  if (name && phone) {
+    const person = new Person({
+      name: name,
+      phone: phone
+    });
+
+    person
+      .save()
+      .then((result) => {
+        console.log(`added ${result.name} ${result.phone} to phonebook`);
+        mongoose.connection.close();
+    });
+  } else {
+    Person
+    .find({})
+    .then((result) => {
+      console.log('phonebook:')
+      result.forEach((person) => {
+        console.log(`${person.name} ${person.phone}`);
+      });
+      mongoose.connection.close();
+    });
+  }
