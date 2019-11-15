e1b4fca0cdec4b6b0ca3675707c3288f56c47025
JanetteDevCode <30553904+JanetteDevCode@users.noreply.github.com>
2019-11-14 20:51:28 -0800

    Add production build of frontend

---
+++ build/precache-manifest.f17a5d3e2bcff07ad035821b89d29b41.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f3eb33f36b09f2bdb5c91a33276c21e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c3c1815d5a21fae38771",
+    "url": "/static/js/2.65ce82f7.chunk.js"
+  },
+  {
+    "revision": "cea85b4bbf54e0f6c102",
+    "url": "/static/js/main.89b131b9.chunk.js"
+  },
+  {
+    "revision": "92e7498acba6e6847f62",
+    "url": "/static/js/runtime-main.bef9a877.js"
+  }
+]);
